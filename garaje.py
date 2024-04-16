a0c671032e7a1a995ff93edd68f5e00fc2bb49bb
mike-bg <mbergesgomez@gmail.com>
2024-04-17 00:39:11 +0200

    prueba de sensor puerta magnética 4

--- garaje.py
+++ garaje.py
@@ -7,22 +7,27 @@ from datetime import datetime
 
 # Configuración de los pines GPIO en modo BOARD
 GPIO.setmode(GPIO.BOARD)
-pin_number = 12  # Por ejemplo, para usar el pin físico 12 de la Raspberry Pi
-GPIO.setup(pin_number, GPIO.IN)  # Configura el pin como entrada
+
+sensorMagnetico = 12  # pin físico 12 de la Raspberry Pi
+GPIO.setup(sensorMagnetico, GPIO.IN)  # Configura el pin como entrada
+
+sensorMagnetico2 = 11  # pin físico 11 de la Raspberry Pi
+GPIO.setup(sensorMagnetico2, GPIO.IN)  # Configura el pin como entrada
 
 try:
     while True:
         # Lee el estado del pin
-        estado = GPIO.input(pin_number)
+        estado = GPIO.input(sensorMagnetico)
 
         # Obtiene la hora actual con segundos
         hora_actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
         # Muestra el estado del pin junto con la hora
-        print(f"{hora_actual} - Estado del pin {pin_number}: {estado}")
+        print(f"{hora_actual} - Estado del pin {sensorMagnetico} (CABLE BLANCO): {estado}")
+        print(f"{hora_actual} - Estado del pin {sensorMagnetico2} (CABLE AZUL): {estado}")
 
         # Espera medio segundo antes de volver a leer
-        time.sleep(0.5)
+        time.sleep(1)
 
 except KeyboardInterrupt:
     # Limpiar los pines GPIO antes de salir si se presiona Ctrl+C
