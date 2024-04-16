39d6add90a3a258b4ab3c01769808ea7647b58db
mike-bg <mbergesgomez@gmail.com>
2024-04-17 00:27:11 +0200

    prueba de sensor puerta magnética 2

--- garaje.py
+++ garaje.py
@@ -3,21 +3,23 @@ import time
 
 # Configuración de los pines GPIO en modo BOARD
 GPIO.setmode(GPIO.BOARD)
-pin12 = 12  # Por ejemplo, para usar el pin físico 12 de la Raspberry Pi
-GPIO.setup(pin12, GPIO.IN)  # Configura el pin como entrada
+pin_number = 12  # Por ejemplo, para usar el pin físico 12 de la Raspberry Pi
+GPIO.setup(pin_number, GPIO.IN)  # Configura el pin como entrada
 
 try:
     while True:
         # Lee el estado del pin
-        estado = GPIO.input(pin12)
+        estado = GPIO.input(pin_number)
 
         # Muestra el estado
-        print("Estado del pin", pin12, ":", estado)
+        print("Estado del pin", pin_number, ":", estado)
 
         # Espera un segundo antes de volver a leer
         time.sleep(1)
 
 except KeyboardInterrupt:
     # Limpiar los pines GPIO antes de salir si se presiona Ctrl+C
+    print("\nPrograma detenido por el usuario.")
+finally:
     GPIO.cleanup()
-    print("\nPines GPIO limpiados.")
+    print("Pines GPIO limpiados.")
