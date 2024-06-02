cd685f97d232f7d67e928f94e8ddcd1ca9063464
mike-bg <mbergesgomez@gmail.com>
2024-06-02 23:18:47 +0200

    prueba de relé 2

--- garaje.py
+++ garaje.py
@@ -4,25 +4,34 @@ import time
 # Configuración de los pines GPIO
 RELAY_PIN_1 = 23
 RELAY_PIN_2 = 24
+IR_SENSOR_PIN = 17
 
 # Configuración de la librería RPi.GPIO
 GPIO.setmode(GPIO.BCM)  # Usamos la numeración BCM
 GPIO.setup(RELAY_PIN_1, GPIO.OUT)  # Configuramos el pin 23 como salida
 GPIO.setup(RELAY_PIN_2, GPIO.OUT)  # Configuramos el pin 24 como salida
+GPIO.setup(IR_SENSOR_PIN, GPIO.IN)  # Configuramos el pin 17 como entrada
 
 try:
     while True:
+        # Leer el estado del sensor de infrarrojos
+        ir_state = GPIO.input(IR_SENSOR_PIN)
+        if ir_state == GPIO.HIGH:
+            print("Objeto detectado")
+        else:
+            print("No se detecta objeto")
+
         # Activar los relés
-        GPIO.output(RELAY_PIN_1, GPIO.HIGH)
-        GPIO.output(RELAY_PIN_2, GPIO.HIGH)
-        print("Relés activados")
-        time.sleep(1)  # Espera 1 segundo
+        #GPIO.output(RELAY_PIN_1, GPIO.HIGH)
+        #GPIO.output(RELAY_PIN_2, GPIO.HIGH)
+        #print("Relés activados")
+        #time.sleep(1)  # Espera 1 segundo
 
         # Desactivar los relés
-        GPIO.output(RELAY_PIN_1, GPIO.LOW)
-        GPIO.output(RELAY_PIN_2, GPIO.LOW)
-        print("Relés desactivados")
-        time.sleep(1)  # Espera 1 segundo
+        #GPIO.output(RELAY_PIN_1, GPIO.LOW)
+        #GPIO.output(RELAY_PIN_2, GPIO.LOW)
+        #print("Relés desactivados")
+        #time.sleep(1)  # Espera 1 segundo
 
 except KeyboardInterrupt:
     # Limpiar los GPIO en caso de interrupción
